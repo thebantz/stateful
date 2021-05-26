f9781b6b0b47b7a63dd418a44d2d308a6d3155a3
“thebantz” <nicoledbanta@gmail.com>
2021-05-26 02:03:55 -0700

    fix AddBlogPost btn to navigate back to index

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,10 +1,11 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
-import { Context } from '../context/BlogContext';
+import React, { useContext, useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   const { addBlogPost } = useContext(Context);
 
   return (
@@ -13,36 +14,39 @@ const CreateScreen = ({ navigation }) => {
       <TextInput
         style={styles.input}
         value={title}
-        onChangeText={text => setTitle(text)}
+        onChangeText={(tot) => setTitle(tot)}
       />
       <Text style={styles.label}>Enter Content:</Text>
       <TextInput
         style={styles.input}
         value={content}
-        onChangeText={text => setContent(text)}
+        onChangeText={(text) => setContent(text)}
       />
       <Button
         title="Add Blog Post"
-        onPress={() => addBlogPost(title, content)}
+        onPress={() =>
+          addBlogPost(title, content, () => {
+            navigation.navigate("Index");
+          })
+        }
       />
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   input: {
     fontSize: 18,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     marginBottom: 15,
     padding: 5,
-    margin: 5
+    margin: 5,
   },
   label: {
     fontSize: 20,
     marginBottom: 5,
-    marginLeft: 5
-  }
+    marginLeft: 5,
+  },
 });
 
 export default CreateScreen;
