bcd21554c8a93456f51bdf007f38615d7eb840ce
“thebantz” <nicoledbanta@gmail.com>
2021-05-25 20:38:24 -0700

    render static data as flatlist

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,7 +4,10 @@ const BlogContext = React.createContext();
 
 // named export, not export default syntax
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+  const blogPosts = [{ title: "Blog Post #1" }, { title: "Blog Post #2" }];
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,13 +1,19 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = ({ navigation }) => {
-  const value = useContext(BlogContext);
+  const blogPosts = useContext(BlogContext);
   return (
     <View>
       <Text>IndexScreen</Text>
-      <Text>{value}</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      ></FlatList>
     </View>
   );
 };
