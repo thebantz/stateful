6c7ca6494364e151f5f7cf9ae0d692bce0a33822
“thebantz” <nicoledbanta@gmail.com>
2021-05-26 01:17:41 -0700

    fix CreateScreen styling

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 
 const CreateScreen = ({ navigation }) => {
@@ -8,13 +8,35 @@ const CreateScreen = ({ navigation }) => {
 
   return (
     <View>
-      <Text>Enter Title:</Text>
-      <TextInput value={title} onChange={(text) => setTitle(text)} />
-      <Text>Enter Content:</Text>
-      <TextInput value={content} onChange={(text) => setContent(text)} />
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChange={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChange={(text) => setContent(text)}
+      />
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
 
 export default CreateScreen;
