a8bc19e0a7b826eecd3d67e090d39702aafebca5
“thebantz” <nicoledbanta@gmail.com>
2021-05-26 00:09:23 -0700

    fix component styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Ionicons } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost } = useContext(Context);
@@ -12,13 +13,33 @@ const IndexScreen = ({ navigation }) => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Ionicons name="trash-outline" style={styles.icon} />
+            </View>
+          );
         }}
       ></FlatList>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "red",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
