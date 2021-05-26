1be738b24d9aba5590069086bd1b1c4cedc7844f
“thebantz” <nicoledbanta@gmail.com>
2021-05-26 00:25:59 -0700

    comp:fix deleteBlogPost functionality

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -11,7 +11,7 @@ import { Context } from "../context/BlogContext";
 import { Ionicons } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
       <Text>IndexScreen</Text>
@@ -25,7 +25,7 @@ const IndexScreen = ({ navigation }) => {
               <Text style={styles.title}>
                 {item.title} - {item.id}
               </Text>
-              <TouchableOpacity onPress={() => console.log(item.id)}>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
                 <Ionicons name="trash-outline" style={styles.icon} />
               </TouchableOpacity>
             </View>
