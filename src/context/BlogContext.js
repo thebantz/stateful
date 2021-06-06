37f5faf9655f0395c39a75e8a0783e7c18bf2267
“thebantz” <nicoledbanta@gmail.com>
2021-06-05 22:34:04 -0700

    delete blogpost

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,15 +7,6 @@ const blogReducer = (state, action) => {
       return action.payload;
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case "edit_blogpost":
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
@@ -43,7 +34,8 @@ const addBlogPost = (dispatch) => {
   };
 };
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
